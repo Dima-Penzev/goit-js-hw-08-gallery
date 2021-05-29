1559848a0569ba7216c5be8055ca651012f72fbc
Dima-Penzev <dima.1301@mail.ru>
2021-05-29 19:45:05 +0300

    homework has done incompletely

--- main.js
+++ main.js
@@ -30,15 +30,52 @@ const cardsMarkup = makeCardsMarkup(pictures);
 
 picturesContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
-picturesContainer.addEventListener('click', e => {
+function openModal(e) {
   e.preventDefault();
   modalWindow.classList.add('is-open');
   bigPicture.setAttribute('src', `${e.target.getAttribute('data-source')}`);
   bigPicture.setAttribute('alt', `${e.target.getAttribute('alt')}`);
-});
+}
 
-closeModalBtn.addEventListener('click', () => {
+function closeModal(e) {
   modalWindow.classList.remove('is-open');
   bigPicture.setAttribute('src', '');
   bigPicture.setAttribute('alt', '');
+}
+
+picturesContainer.addEventListener('click', openModal);
+
+closeModalBtn.addEventListener('click', closeModal);
+
+window.addEventListener('keydown', e => {
+  if (e.code === 'Escape') {
+    closeModal(e);
+  }
 });
+
+modalWindow.addEventListener('click', (e) => {
+  if (e.target.nodeName === 'DIV') {
+    closeModal(e);
+  }
+})
+
+
+let currentIndex = 0;
+window.addEventListener('keydown', e => {
+  if (e.code === 'ArrowRight') {
+    currentIndex += 1;
+  } else if (e.code === 'ArrowLeft') {
+    currentIndex -= 1;
+  }
+
+  console.log(currentIndex);
+
+  pictures.find((picture, index) => {
+  if (e.target.getAttribute('href') === picture.original) {
+    console.log(currentIndex = index);
+  }
+});
+});
+
+
+
