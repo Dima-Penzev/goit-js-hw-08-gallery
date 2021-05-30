3c1c0ca87a4c67a3e1618c4856503c8a29c5eff6
Dima-Penzev <dima.1301@mail.ru>
2021-05-30 18:36:28 +0300

    hw was complited

--- main.js
+++ main.js
@@ -5,20 +5,22 @@ const picturesContainer = document.querySelector('.js-gallery');
 const modalWindow = document.querySelector('.js-lightbox');
 const closeModalBtn = document.querySelector('[data-action="close-lightbox"]');
 const bigPicture = document.querySelector('.lightbox__image');
+const modalOverlay = document.querySelector('.lightbox__overlay');
 
 function makeCardsMarkup(pictures) {
   return pictures
-    .map(({ preview, original, description }) => {
+    .map(({ preview, original, description }, index) => {
       return `<li class="gallery__item">
   <a
     class="gallery__link"
     href="${original}"
-  >
+    >
     <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    data-index="${index}"
     />
   </a>
 </li>`;
@@ -31,16 +33,27 @@ const cardsMarkup = makeCardsMarkup(pictures);
 picturesContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
 function openModal(e) {
-  e.preventDefault();
-  modalWindow.classList.add('is-open');
-  bigPicture.setAttribute('src', `${e.target.getAttribute('data-source')}`);
-  bigPicture.setAttribute('alt', `${e.target.getAttribute('alt')}`);
+  currentIndex = Number(e.target.getAttribute('data-index'));
+  console.log(currentIndex);
+
+  if (e.target.getAttribute('class') !== 'gallery__image') {
+    return;
+  } else {
+    e.preventDefault();
+    modalWindow.classList.add('is-open');
+    bigPicture.setAttribute('src', e.target.getAttribute('data-source'));
+    bigPicture.setAttribute('alt', e.target.getAttribute('alt'));
+
+    modalOverlay.addEventListener('click', closeModal);
+  }
 }
 
-function closeModal(e) {
+function closeModal() {
   modalWindow.classList.remove('is-open');
   bigPicture.setAttribute('src', '');
   bigPicture.setAttribute('alt', '');
+
+  modalOverlay.removeEventListener('click', closeModal);
 }
 
 picturesContainer.addEventListener('click', openModal);
@@ -49,16 +62,9 @@ closeModalBtn.addEventListener('click', closeModal);
 
 window.addEventListener('keydown', e => {
   if (e.code === 'Escape') {
-    closeModal(e);
+    closeModal();
   }
 });
-
-modalWindow.addEventListener('click', (e) => {
-  if (e.target.nodeName === 'DIV') {
-    closeModal(e);
-  }
-})
-
 
 let currentIndex = 0;
 window.addEventListener('keydown', e => {
@@ -68,14 +74,15 @@ window.addEventListener('keydown', e => {
     currentIndex -= 1;
   }
 
-  console.log(currentIndex);
+  setPicture(currentIndex);
+});
 
+function setPicture(indexOfPicture) {
   pictures.find((picture, index) => {
-  if (e.target.getAttribute('href') === picture.original) {
-    console.log(currentIndex = index);
-  }
-});
-});
-
-
-
+    if (indexOfPicture === index) {
+      console.log(index);
+      bigPicture.setAttribute('src', pictures[index].original);
+      bigPicture.setAttribute('alt', pictures[index].description);
+    }
+  });
+}
